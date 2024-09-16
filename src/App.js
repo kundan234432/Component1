72f859d4ff1482275984b792d0052e32fb6ac8b7
kundan234432 <kundan234432@gmail.com>
2024-09-16 16:12:48 +0530

    Initial Commmit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1>Welcome to My First React App!</h1>
+    <p>This is a basic React component.</p>
+  </div>
+  );
+}
+
+export default App;
